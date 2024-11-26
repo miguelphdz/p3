76b5866d78c2b76e74b156ee1e6620c867c3a2ac
benjaAmin777 <bensaibort@gmail.com>
2024-11-25 20:59:16 -0600

    trabajos realizado
    ya funciona de forma dinamica, en base a la base de datos

--- js/productos_hechos.js
+++ js/productos_hechos.js
@@ -2,10 +2,11 @@
 const SUPABASE_URL = 'https://vnkohvizfizhindrprxj.supabase.co'; // Reemplaza con la URL de tu proyecto Supabase
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZua29odml6Zml6aGluZHJwcnhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1Mzk2NzQsImV4cCI6MjA0NzExNTY3NH0.bdnM5VJcQqsMPSQgdLmZQ-d6OLxT1Je-s5N2uPgWWAw'; // Reemplaza con tu clave API pública
 const TABLE_NAME = 'producto'; // Nombre de la tabla
+
 // Función para hacer una consulta GET a la tabla 'producto'
 async function getData() {
     try {
-        const response = await fetch(`${SUPABASE_URL}/rest/v1/${TABLE_NAME}?select=descripcion,precio,imagen_url`, {
+        const response = await fetch(`${SUPABASE_URL}/rest/v1/${TABLE_NAME}?select=descripcion,precio,imagen_url&id=gte.1&id=lte.8`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -32,7 +33,7 @@ async function getData() {
 
 // Función para mostrar los datos en la página
 function displayData(data) {
-    const container = document.getElementById('grid');
+    const container = document.getElementById('productos-hechos');
     container.innerHTML = ''; // Limpia el contenedor
 
     data.forEach(item => {
