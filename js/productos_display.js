6522799aac26a40595f1f8bfcf833479099fcc41
benjaAmin777 <bensaibort@gmail.com>
2024-11-26 23:07:50 -0600

    punto de incio
    el punto de inicio de hoy, guardo por si la cago

--- js/productos_display.js
+++ js/productos_display.js
@@ -3,10 +3,10 @@ const SUPABASE_URL = 'https://vnkohvizfizhindrprxj.supabase.co'; // Reemplaza co
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZua29odml6Zml6aGluZHJwcnhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1Mzk2NzQsImV4cCI6MjA0NzExNTY3NH0.bdnM5VJcQqsMPSQgdLmZQ-d6OLxT1Je-s5N2uPgWWAw'; // Reemplaza con tu clave API pública
 const TABLE_NAME = 'producto'; // Nombre de la tabla
 
-// Función para hacer una consulta GET a la tabla 'producto' con filtro de id > 9
+// Función para hacer una consulta GET a la tabla 'producto' con filtro de id > 9 y disponible = true
 async function getData() {
     try {
-        const response = await fetch(`${SUPABASE_URL}/rest/v1/${TABLE_NAME}?select=*&id=gt.9`, {
+        const response = await fetch(`${SUPABASE_URL}/rest/v1/${TABLE_NAME}?select=*&id=gt.9&disponible=eq.true`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
